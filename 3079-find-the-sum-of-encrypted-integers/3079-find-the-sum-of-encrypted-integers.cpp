f311400ac18e9576aa88cb9c942a8dfc2d2e986e
Shubh Garg <shubhgarg1113@gmail.com>
2026-07-10 09:25:11 +0530

    Time: 3 ms (84.12%), Space: 27.8 MB (74.23%) - LeetHub

--- 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.cpp
+++ 3079-find-the-sum-of-encrypted-integers/3079-find-the-sum-of-encrypted-integers.cpp
@@ -4,23 +4,21 @@ public:
         int res = 0;
         for(int i = 0; i < nums.size(); i++)
         {
-            int max = 0;
+            int max_d = 0;
             int temp = nums[i];
             int number = 0;
             int count = 0;
             while(temp>0){
                 count++;
                 int digit = temp % 10;
-                if(max < digit){
-                    max = digit;
-                }
+                max_d = max(max_d,digit);
                 temp /= 10;
             }
             while(count >= 0){
                 number += pow(10,count - 1);
                 count--;
             }
-            res += number * max;
+            res += number * max_d;
         }
         return res;
     }
